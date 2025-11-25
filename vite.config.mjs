71b1457f2c0482a5f357f42487505ead150c9d01
macamala <79318740+macamala@users.noreply.github.com>
2025-11-25 20:57:00 +1100

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    host: "0.0.0.0",
+    port: 8080
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 8080,
+    allowedHosts: [
+      "allball-frontend-production.up.railway.app"
+    ]
   }
 });
