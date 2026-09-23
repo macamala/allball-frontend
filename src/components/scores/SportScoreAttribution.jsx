9f2d5a3076de3f0ced3c52fa4b8965d839740afb
macamala <79318740+macamala@users.noreply.github.com>
2026-09-23 21:26:01 +1000

    Delete public SportScore attribution component

--- src/components/scores/SportScoreAttribution.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function SportScoreAttribution() {
-  return (
-    <p className="sports-data-attribution">
-      <a href="https://sportscore.com/">Powered by SportScore</a>
-    </p>
-  );
-}
